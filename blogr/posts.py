12b09cc66071a341a1b80f431a59ef17e11d2319
Gaytan Padilla Gilberto (JuP1/HRL22) <ggi2ju@bosch.com>
2025-08-26 14:05:22 -0600

    Terminando seccion de guardar y eliminar, continuar con visualizacion de posts

--- blogr/posts.py
+++ blogr/posts.py
@@ -12,11 +12,58 @@ def posts():
     posts = Post.query.all()
     return render_template('admin/posts.html', posts = posts)
 
-@bp.route('/create')
+@bp.route('/create', methods=('GET', 'POST'))
+@login_required
 def create():
-    return 'Pagina de create'
+    if request.method == 'POST':
+        url = request.form.get('url')
+        url = url.replace('', '-')
+        title = request.form.get('title')
+        info = request.form.get('info')
+        content = request.form.get('ckeditor')
 
-@bp.route('/update')
-def update():
-    return 'Pagina de update'
+        post = Post(g.user.id, url, title, info, content)
+
+        error = None
+
+        #Comparando url de post con los existentes
+        post_url = Post.query.filter_by(url = url).first()
+        if post_url == None:
+            db.session.add(post)
+            db.session.commit()
+            flash(f'El blog {post.title} se registro correctamente')
+            return redirect(url_for('post.posts'))
+        else:
+            error = f'El URL {url} ya esta registrado'
+        flash(error)
+    return render_template('admin/create.html')
+
+@bp.route('/update/<int:id>', methods = ('GET','POST'))
+@login_required
+def update(id):
+    post = Post.query.get_or_404(id)
+    print("sigue aqui")
+    if request.method == 'POST':
+
+        post.title = request.form.get('title')
+        post.info = request.form.get('info')
+        post.content = request.form.get('ckeditor')
+
+        db.session.commit()
+        flash(f"El blog {post.title} se actualizo correctamente")
+        return redirect(url_for('post.posts'))
+    
+    return render_template('admin/update.html', post = post)
+
+
+@bp.route('/delete/<int:id>')
+@login_required
+def delete(id):
+    post = Post.query.get_or_404(id)
+    db.session.delete(post)
+    db.session.commit()
+    flash(F'El blog {post.title} se elimino correctamente')
+
+    return redirect(url_for('post.posts'))
+
 
